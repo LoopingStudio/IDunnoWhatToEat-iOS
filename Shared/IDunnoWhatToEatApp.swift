7228680067d0acedf28bd123a13ceaa555db23a1
Kelian <kelian@loungeup.com>
2021-01-13 12:08:18 +0100

    Initial Commit

---
+++ Shared/IDunnoWhatToEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDunnoWhatToEatApp.swift
+//  Shared
+//
+//  Created by Kelian Daste on 13/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct IDunnoWhatToEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
